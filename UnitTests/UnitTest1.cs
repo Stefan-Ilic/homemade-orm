077504614d6786c8e9f82fd5887b59ed08a951a9
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-09-13 10:35:48 +0200

    Add Visual Studio project

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Shouldly;
+using Xunit;
+
+namespace UnitTests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Empty_Must_Succeed()
+    {
+      (true).ShouldBeTrue();
+    }
+  }
+}
