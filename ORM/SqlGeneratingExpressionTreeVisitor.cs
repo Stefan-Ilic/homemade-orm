371bdabe11bd3282a3be0d7e6eaa86fe156b48a3
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-10-02 16:45:27 +0200

    Add real select statement building

--- ORM/SqlGeneratingExpressionTreeVisitor.cs
+++ ORM/SqlGeneratingExpressionTreeVisitor.cs
@@ -4,44 +4,84 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
+using Interfaces;
 using ORM.Attributes;
 
 namespace ORM
 {
     internal class SqlGeneratingExpressionTreeVisitor : ExpressionTreeVisitor
     {
-        public SqlStatementBuilder SqlStatementBuilder = new SqlStatementBuilder();
+        public SqlStatementBuilder SqlStatementBuilder = new SqlStatementBuilder(SqlStatementType.Select);
+        public StringBuilder StringBuilder = new StringBuilder();
 
         public override Expression Visit(Expression e)
         {
             return base.Visit(e);
         }
 
+        protected override Expression VisitMemberAccess(MemberExpression m)
+        {
+            // TODO prefer Column Atribute, if not set, use prop name
+            SqlStatementBuilder.AddColumnToCondition(m.Member.Name);
+            return base.VisitMemberAccess(m);
+        }
+
         protected override Expression VisitConstant(ConstantExpression c)
         {
-            var className = c?.Value.GetType().GetGenericArguments().First().Name;
-
-            var genericArgument = c?.Value.GetType().GetGenericArguments().FirstOrDefault();
-
-            var tableAttributeName = "";
-            var tableAttribute = genericArgument.GetCustomAttribute<TableAttribute>();
-            if (tableAttribute != null)
+            if (!c.Value.GetType().IsPrimitive && !(c.Value is string)) //TODO decimal etc aint primitive either
             {
-                tableAttributeName = tableAttribute.TableName;
+                var className = c.Value.GetType().GetGenericArguments().First().Name;
+
+                var typeOfObjectInTable = c.Value.GetType().GetGenericArguments().FirstOrDefault();
+
+                var tableAttributeName = "";
+                var tableAttribute = typeOfObjectInTable.GetCustomAttribute<TableAttribute>();
+                if (tableAttribute != null)
+                {
+                    tableAttributeName = tableAttribute.TableName;
+                }
+
+                SqlStatementBuilder.TableName = tableAttributeName != "" ? tableAttributeName : className;
+
+                var properties = typeOfObjectInTable?.GetProperties();
+
+                //fetch properties for column names
+                //TODO check for ColumnAttribute, take prop name if none assigned
+                if (properties != null)
+                {
+                    foreach (var property in properties)
+                    {
+                        SqlStatementBuilder.ColumnNames.Add(property.Name);
+                    }
+                }
+            }
+            else
+            {
+                switch (c.Value)
+                {
+                    case string s:
+                        SqlStatementBuilder.AddStringToCondition(s);
+                        break;
+                    case int i:
+                        SqlStatementBuilder.AddIntToCondition(i);
+                        break;
+                }
             }
 
-            SqlStatementBuilder.TableName = tableAttributeName != "" ? tableAttributeName : className;
-            Console.WriteLine(SqlStatementBuilder.TableName);
 
             return base.VisitConstant(c);
         }
 
         protected override Expression VisitBinary(BinaryExpression b)
         {
-            // Console.WriteLine(b.Type);
-            // Console.WriteLine($"  {Visit(b.Left)} {b.NodeType} {Visit(b.Right)}");
+            SqlStatementBuilder.StartCondition();
+            Visit(b.Left);
+            SqlStatementBuilder.AddBinaryOperatorToCondition(b.NodeType);
+            Visit(b.Right);
+            SqlStatementBuilder.EndCondition();
             return b;
-            // return base.VisitBinary(b);
         }
+
+
     }
 }
--- ORM/SqlStatementBuilder.cs
+++ ORM/SqlStatementBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using Interfaces;
 
@@ -14,14 +15,86 @@ namespace ORM
                 switch (StatementType)
                 {
                     case SqlStatementType.Select:
-                        return $"SELECT {string.Join(",", ColumnNames)} FROM {TableName}";
+                        var selectbuilder = new StringBuilder();
+                        selectbuilder.Append("SELECT ");
+                        selectbuilder.Append(string.Join(",", ColumnNames));
+                        selectbuilder.Append(" FROM ");
+                        selectbuilder.Append(TableName);
+                        if (_hasWhereClause)
+                        {
+                            selectbuilder.Append(" WHERE ");
+                            selectbuilder.Append(_whereClauseBuilder.ToString());
+                        }
+
+                        return selectbuilder.ToString();
                     default: throw new Exception();
                 }
             }
         }
 
+        public SqlStatementBuilder(SqlStatementType statementType)
+        {
+            StatementType = statementType;
+        }
+
+        private bool _hasWhereClause;
+
+        private readonly StringBuilder _whereClauseBuilder = new StringBuilder();
+
+        public void StartCondition()
+        {
+            _whereClauseBuilder.Append("(");
+            _hasWhereClause = true;
+        }
+
+        public void EndCondition()
+        {
+            _whereClauseBuilder.Append(")");
+        }
+
+        public void AddBinaryOperatorToCondition(ExpressionType expressionType)
+        {
+            _whereClauseBuilder.Append(GetBinaryExpressionSymbol(expressionType));
+        }
+
+        public void AddStringToCondition(string stringToBeAdded)
+        {
+            _whereClauseBuilder.Append("'");
+            _whereClauseBuilder.Append(stringToBeAdded);
+            _whereClauseBuilder.Append("'");
+        }
+
+        public void AddIntToCondition(int intToBeAdded)
+        {
+            _whereClauseBuilder.Append(intToBeAdded);
+        }
+
+        public void AddColumnToCondition(string columnToBeAdded)
+        {
+            _whereClauseBuilder.Append(columnToBeAdded);
+        }
+
         public SqlStatementType StatementType { get; set; }
         public string TableName { get; set; }
-        public IList<string> ColumnNames { get; set; }
+        public IList<string> ColumnNames { get; set; } = new List<string>();
+
+        public string GetBinaryExpressionSymbol(ExpressionType expressionType)
+        {
+            switch (expressionType)
+            {
+                case ExpressionType.AndAlso:
+                    return " AND ";
+                case ExpressionType.Equal:
+                    return " = ";
+                case ExpressionType.GreaterThan:
+                    return " > ";
+                case ExpressionType.LessThan:
+                    return " < ";
+                case ExpressionType.OrElse:
+                    return " OR ";
+                default:
+                    return "";
+            }
+        }
     }
 }
