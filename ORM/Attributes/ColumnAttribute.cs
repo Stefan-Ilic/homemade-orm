ca2d1b96b481f73f375960a22d1318c65d84b4b7
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-10-02 11:02:32 +0200

    Add custom attributes for Table and Column

---
+++ ORM/Attributes/ColumnAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ORM.Attributes
+{
+    public class ColumnAttribute : Attribute
+    {
+        public ColumnAttribute(string name = "")
+        {
+            
+        }
+    }
+}
---
+++ ORM/Attributes/TableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ORM.Attributes
+{
+    public class TableAttribute : Attribute
+    {
+        public TableAttribute(string name = "")
+        {
+            TableName = name;
+        }
+
+        public string TableName { get; set; }   
+    }
+}
